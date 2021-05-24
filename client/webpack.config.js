d22743db390449a6c7eec5b57043ee0d61e3ab4c
hayri <hayriyigit@protonmail.com>
2021-05-24 10:54:46 +0300

    add webpack clean and html plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let mode = 'development';
 let target = 'web';
@@ -13,6 +16,7 @@ module.exports = {
   target: target,
 
   output: {
+    path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'images/[hash][ext][query]',
   },
 
@@ -40,7 +44,13 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
   resolve: {
     extensions: ['js', 'jsx'],
   },
